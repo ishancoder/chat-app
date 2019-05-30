3eb972bd663086cfbb08cd93297c61aa68fc3230
ishancoder <isaxena171@gmail.com>
2019-05-30 21:30:54 +0530

    Completed the implementation of mock APIs

---
+++ src/api/api.js
@@ -0,0 +1,83 @@
+import {v4 as uuid} from "uuid";
+
+let users = {};
+let chats = {};
+const FAILED = "FAILED";
+const SUCCESS = "SUCCESS";
+
+// Utility functions
+// All the UTILITY functions assume the best case scenario all the error handling should be done in the Mock API functions.
+function exists(userName) {
+    return !!users[userName];
+}
+
+function areConnected(userName, contactUserName) {
+    return users[userName] && users[userName].contacts && !!users[userName].contacts.find(contact => contact.userName === contactUserName);
+}
+
+function connect(userName, contactUserName) {
+    if(exists(userName) && exists(contactUserName)) {
+        const chatId = uuid();
+        users[userName].contacts.push({chatId, userName: contactUserName});
+        users[contactUserName].contacts.push({chatId, userName});
+    }
+}
+
+function getChatId(userName, contactUserName) {
+    return users[userName].contacts.find(contact => contact.userName === contactUserName).chatId;
+}
+
+
+// Mock API functions for USER
+export function addUser(userName) {
+    if(exists(userName)) {
+        return Promise.reject({status: FAILED, err: "User with this name already exists"});
+    } 
+    users[userName] = {contacts: []};
+    return Promise.resolve({status: SUCCESS, data: {userName, contacts:[]}});
+}
+
+export function userDoesExists(userName) {
+    return Promise.resolve({exists: exists(userName)});
+}
+
+export function addContact(userName, contactUserName) {
+    if(!exists(userName))
+        return Promise.reject({err: `No user exists with username : ${userName}`});
+    if(!exists(contactUserName))
+        return Promise.reject({err: `No user exists with username ${contactUserName}`});
+    if(areConnected(userName, contactUserName)) 
+        return Promise.reject({err: `Users are already connected`});
+
+    connect(userName, contactUserName);
+    return Promise.resolve({status: SUCCESS, data: "Succefully connected"});
+}
+
+export function getUser(userName) {
+    if(!exists(userName))
+        return Promise.reject({status: FAILED, err: `No user exists with username: ${userName}`});
+    const {contacts} = users[userName];
+    return Promise.resolve({status: SUCCESS, data: {userName, contacts}});
+}
+
+// Mock API functions for CHATS
+export function sendMessage(from, to, message) {
+    if(!exists(from))
+        return Promise.reject({status: FAILED, err: `Invalid username : ${from}`});
+    if(!exists(to))
+        return Promise.reject({status: FAILED, err: `Invalid username : ${to}`});
+    if(!areConnected(from, to))
+        return Promise.reject({status: FAILED, err: `Users ${from} and ${to} are not connected`});
+    const chatId = getChatId(from, to);
+    const messageBody = {message, createdOn: (new Date()).getTime(), from, to};
+    if(!chats[chatId]) {
+        chats[chatId].push(messageBody);
+    } else {
+        chats[chatId] = [messageBody];
+    }
+    return Promise.resolve({status: SUCCESS, data: "Message sent successfully"}); 
+}
+
+export function sendAudioMessage(from, to, audioObject) {
+    //TODO: Implement this after finishing the basic chat flow
+}
